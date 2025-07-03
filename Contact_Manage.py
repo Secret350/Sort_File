750246802f3c7e71bec38c55ceb54def08723a2f
Contact <minhtri13112006@gmail.com>
2025-07-03 22:57:01 +0700

    3/7/2025

--- Contact_Manage.py
+++ Contact_Manage.py
@@ -1,3 +1,5 @@
+import sys
+
 #File
 
 RfileP = open("Contact-Phone_number.txt", "r+")
@@ -14,14 +16,20 @@ namedic = dict(zip(phonequan,phonein))
 #Ham chon phuong thuc
 
 def method():
-	mthd = input("Choose the method you want: ADD | DELETE | SHOWINFO")
+	mthd = input("Choose the method you want: ADD | DELETE | SHOWINFO\n")
 	return mthd
+
+#Ham nhap so dien thoai lien he
+
+def num():
+	key = str(input("Please enter the phone number!"))
+	return key
 
 #Ham them dia chi vao danh ba
 
-def add():
+def add(phonenum):
 	newcontact_name = str(input("Please enter new contact's name: "))
-	newcontact_phone = str(input("Please enter new contact's phonenumber: "))
+	newcontact_phone = phonenum
 	newcontact_email = str(input("Please enter new contact's email: "))
 	if len(namedic) == 0:
 		namedic[int(newcontact_phone)] = 0
@@ -47,20 +55,42 @@ class contact:
 	def show_info(self):
 		print(f"The contact's infomation:\nName: {self.name}Phone_Number: {self.phonenumber}\nEmail: {self.email}")
 
+#Ham xoa dia chi lien he
+
+#def delete(phonenumber):
+
+
 #Cac thao tac
+
+phonenum = num()
+
+if int(phonenum) not in phonequan:
+	print("This is newphone number do you want to add new?")
+	ans = input("Yes/EnterAgain/End").upper()
+	if ans == "YES":
+		add(phonenum)
+	elif ans == "ENTERAGAIN":
+		phonenum = num()
+	else:
+		sys.exit()
+
+#Chon chuc nang
+
 RfileN.seek(0)
 nameread = RfileN.readlines()
 RfileE.seek(0)
 mailread = RfileE.readlines()
-
-key =  str(input("Please enter the phone number!"))
-
-if
-	
-#Chon chuc nang
+cont = contact(nameread[namedic[int(phonenum)]],phonenum,mailread[namedic[int(phonenum)]])
 
 mthd = method()
 
+if mthd == "ADD":
+	add()
+elif mthd == "DELETE":
+	delete()
+else:
+	cont.show_info()
 
-cont = contact(nameread[namedic[int(key)]],key,mailread[namedic[int(key)]])
-cont.show_info()
+
+
+
